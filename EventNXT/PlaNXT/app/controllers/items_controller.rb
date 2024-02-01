e626e4f2a75aafe54ebae6a05f1dc2c52ab663b7
alex <alexhwise@yahoo.com>
2024-02-01 09:19:31 -0600

    event

---
+++ EventNXT/PlaNXT/app/controllers/items_controller.rb
@@ -0,0 +1,48 @@
+class ItemsController < ApplicationController
+  before_action :set_item, only: %i[ show edit update destroy ]
+
+  # POST /items or /items.json
+  def create
+    @item = Item.new(item_params)
+
+    if @item.save
+      render json: @item, status: :ok
+    else
+      render json: @item.errors, status: :unprocessable_entity
+    end
+    
+  end
+
+  # search for items by step_id
+  def search
+    @items = Item.where(step_id: params[:step_id])
+    render json: @items
+  end
+  
+
+  # PATCH/PUT /items/1 or /items/1.json
+  def update
+    if @item.update(item_params)
+      render json: @item, status: :ok
+    end
+  end
+
+  # DELETE /items/1 or /items/1.json
+  def destroy
+    @item.destroy
+    respond_to do |format|
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_item
+      @item = Item.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def item_params
+      params.require(:item).permit(:name, :model, :width, :length, :depth, :rotation, :description, :xpos, :ypos, :zpos, :step_id, :setup_start_time, :setup_end_time, :breakdown_start_time, :breakdown_end_time)
+    end
+end
---
+++ EventNXT/PlaNXT/db/schema.rb
@@ -0,0 +1,79 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2023_11_29_104030) do
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.string "model"
+    t.float "width"
+    t.float "length"
+    t.float "depth"
+    t.float "rotation"
+    t.string "description"
+    t.float "xpos"
+    t.float "ypos"
+    t.float "zpos"
+    t.integer "step_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.datetime "setup_start_time"
+    t.datetime "setup_end_time"
+    t.datetime "breakdown_start_time"
+    t.datetime "breakdown_end_time"
+  end
+
+  create_table "plans", force: :cascade do |t|
+    t.string "name"
+    t.string "owner"
+    t.float "venue_length"
+    t.float "venue_width"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.string "timezone"
+    t.index ["user_id"], name: "index_plans_on_user_id"
+  end
+
+  create_table "steps", force: :cascade do |t|
+    t.date "start_date"
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.datetime "break1_start_time"
+    t.datetime "break1_end_time"
+    t.datetime "break2_start_time"
+    t.datetime "break2_end_time"
+    t.integer "plan_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.integer "level"
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest"
+    t.string "viewPermission"
+    t.string "editPermission"
+    t.text "description"
+    t.string "extra1"
+    t.string "extra2"
+    t.string "extra3"
+    t.boolean "enabled", default: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email"
+  end
+
+  add_foreign_key "items", "steps"
+  add_foreign_key "plans", "users"
+  add_foreign_key "steps", "plans"
+end
